1e132d1f7a16c5152118d298d63010848b64af0a
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-10-03 16:39:33 +0100

    typed array

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,11 @@
+export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  try {
+    const view = new DataView(buffer, position);
+    view.setInt8(0, value);
+  } catch (err) {
+    throw new Error("Position outside range");
+  }
+
+  return new DataView(buffer);
+}
