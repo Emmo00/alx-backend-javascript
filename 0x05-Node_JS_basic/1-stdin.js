27afb9bf8014ff330684fa332c989170b2d58bc9
Emmanuel Nwafor@ubuntuVM <nwaforemmanuel005@gmail.com>
2023-12-19 05:52:21 +0100

    ggg

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,22 @@
-console.log('Welcome to Holberton School, what is your name?');
+const readline = require('readline');
 
-process.stdin.on('data', (name) => {
-  process.stdout.write(`Your name is: ${name.toString('utf-8')}`);
-  console.log('This important software is now closing ');
-  process.exit();
+// Create an interface for reading user input
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// Display welcome message and prompt for name
+rl.question('Welcome to Holberton School, what is your name? ', (name) => {
+  // Respond with personalized message
+  console.log(`Your name is: ${name}`);
+
+  // Close the interface and display closing message
+  rl.close();
+  console.log('This important software is now closing\n');
+});
+
+// Handle unexpected errors
+rl.on('error', (err) => {
+  console.error(err.message);
 });
