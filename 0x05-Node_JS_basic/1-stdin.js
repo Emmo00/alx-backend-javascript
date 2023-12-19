e8fe7e0fd02fee6fda5b2a84d92bb295128fca2f
Emmanuel Nwafor@ubuntuVM <nwaforemmanuel005@gmail.com>
2023-12-19 15:40:44 +0100

    odo

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,6 +2,9 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
-  if (name) process.stdout.write(`Your name is: ${name.toString('utf-8')}`);
-  console.log('This important software is now closing ');
+  if (name) process.stdout.write(`Your name is: ${name}`);
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
