b36c101e45fc6fbeddaea6f2fe48c0ffac9f2055
Emmanuel Nwafor@ubuntuVM <nwaforemmanuel005@gmail.com>
2023-12-22 12:04:26 +0100

    eslint

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,19 +1,18 @@
 const fs = require('fs');
 
-function countStudents (path) {
+function countStudents(path) {
   if (!fs.existsSync(path)) {
     throw new Error('Cannot load the database');
   }
   const file = fs.readFileSync(path);
   const lines = file.toString().trim().split('\n').slice(1);
   const fields = {};
-  for (let i = 0; i < lines.length; i++) {
+  for (let i = 0; i < lines.length; i + 1) {
     if (lines[i].length < 1) {
       lines.splice(i, 1);
     }
   }
   for (const line of lines) {
-    if (!line) continue;
     const field = line.split(',')[3];
     const name = line.split(',')[0];
     if (fields[field]) {
